becd36d1378bcec3b874c21b2b6f3258cccc3caf
Rafael Carvalho <rpaschoal@live.com>
2019-01-17 09:14:57 +1300

    Fix issue with not being able to NPM link on offline bot project

--- src/spec/components/ng-chat-options.component.spec.ts
+++ src/spec/components/ng-chat-options.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NgChatOptionsComponent } from './ng-chat-options.component';
+import { NgChatOptionsComponent } from '../../ng-chat/components/ng-chat-options/ng-chat-options.component';
 
 describe('NgChatOptionsComponent', () => {
   let component: NgChatOptionsComponent;
